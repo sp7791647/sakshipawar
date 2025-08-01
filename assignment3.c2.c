dc7d473b3925702fe4bf765b6e6e0388ca1fc604
sp7791647 <sp7791647@gmail.com>
2025-08-01 10:10:28 -0700

    Add files via upload

---
+++ assignment3.c2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int start=1, end=5;
+	
+	printf("Enter the starting number:");
+	scanf("%d",&start);
+	printf("Enter the ending number:");
+	scanf("%d",&end);
+	printf("\neven numbers between %d and %d are:\n",start,end);{
+	for(i = start;i<=end;i++)
+	if(i % 2== 0
+	printf("%d",i);
+}
+	printf("\odd numbers between %d and %d are:\n",start,end);
+	for(i = start;i<=end;i++)
+	if(i % 2!= 0
+	printf("%d",i);	
+
+}
+}
